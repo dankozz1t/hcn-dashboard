4c90ec03ae802993d39cd1024e04bba0522769a1
Alex-Taylor Danko <AlexDankoXXL@gmail.com>
2023-11-09 11:01:13 +0200

    add fetch all players

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import Table from "@/components/Table.vue";
+import { ref, onMounted } from "vue";
+
+const playersData = ref(null);
+
+onMounted(async () => {
+  await fetch(
+    "https://hcn-rating-system-e3c67355a6ec.herokuapp.com/api/v1/players/"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      playersData.value = data;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+</script>
+
+<template>
+  <div>
+    <h1>Dashboard</h1>
+
+    <Table v-if="playersData" :players="playersData" />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Table.vue
@@ -0,0 +1,48 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>Global Rank</th>
+        <th>Nickname</th>
+        <th>Rating</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(player, index) in players" :key="index">
+        <td>{{ player[0] }}</td>
+        <td>{{ player[1] }}</td>
+        <td>{{ player[2] }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const { players } = defineProps(["players"]);
+</script>
+
+<style scoped>
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+thead tr th {
+  color: black;
+}
+
+th,
+td {
+  border: 1px solid #ddd;
+  padding: 8px;
+  text-align: left;
+
+  color: white;
+}
+
+th {
+  background-color: #f2f2f2;
+}
+</style>
