3d1d62f34bd8c2e259aed98506b4db1da2116705
Alex-Taylor Danko <AlexDankoXXL@gmail.com>
2023-12-02 21:42:37 +0200

    update gif, add other skills and combinations

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -10,6 +10,19 @@ const abilities = ref([
     demo: "../spells/deflect.gif",
   },
   {
+    name: "about:blink",
+    type: "about:attack-defense",
+    description: "about:blink:description",
+    spells: ["../spells/blink.jpg"],
+    demo: "../spells/blink.gif",
+  },
+  {
+    name: "about:orb-of-preparation",
+    type: "about:attack",
+    description: "about:orb-of-preparation:description",
+    spells: ["../spells/refresh.jpg"],
+  },
+  {
     name: "about:sword-wave",
     type: "about:attack",
     description: "about:sword-wave:description",
@@ -23,12 +36,48 @@ const abilities = ref([
     spells: ["../spells/dagger2.jpg"],
     demo: "../spells/dagger.gif",
   },
+]);
+
+const combinations = ref([
+  {
+    name: "about:double-blink",
+    type: "",
+    spells: [
+      "../spells/blink.jpg",
+      "../spells/refresh.jpg",
+      "../spells/blink.jpg",
+    ],
+    demo: "../spells/DB.gif",
+  },
+  {
+    name: "about:wr",
+    type: "",
+    spells: ["about:hit", "../spells/dagger2.jpg"],
+    demo: "../spells/WR.gif",
+  },
   {
     name: "about:predict-wave",
     type: "",
     description: "about:predict-wave:description",
     spells: ["../spells/wave.jpg", "../spells/eye.jpg"],
     demo: "../spells/predict_wave.gif",
+  },
+  {
+    name: "about:chop",
+    type: "",
+    spells: ["../spells/blink.jpg", "about:hit"],
+    demo: "../spells/chop.gif",
+  },
+  {
+    name: "about:double-blink-chop",
+    type: "",
+    spells: [
+      "../spells/blink.jpg",
+      "../spells/refresh.jpg",
+      "../spells/blink.jpg",
+      "about:hit",
+    ],
+    demo: "../spells/dbchop.gif",
   },
 ]);
 </script>
@@ -40,7 +89,7 @@ const abilities = ref([
     <p>{{ $t("about:description") }}</p>
     <p>{{ $t("about:condition") }}</p>
 
-    <h3>{{ $t("about:abilities") }}:</h3>
+    <h3 class="title mt-2">{{ $t("about:abilities") }}:</h3>
     <ul class="abilities">
       <li class="ability" v-for="ability in abilities" :key="ability.name">
         <div class="ability__box">
@@ -51,18 +100,64 @@ const abilities = ref([
         </div>
 
         <ul class="ability__box">
-          <li v-for="spell in ability.spells" :key="spell">
+          <li v-for="(spell, index) in ability.spells" :key="spell">
             <div class="ability__spell">
               <img
+                v-if="spell.includes('/')"
                 class="ability__spell-img"
                 :src="spell"
                 :alt="ability.name"
               />
+              <p v-else class="ability__spell-hit">{{ $t(spell) }}</p>
+
+              <p
+                v-if="index < ability.spells.length - 1"
+                class="ability__spell-plus"
+              >
+                +
+              </p>
             </div>
           </li>
         </ul>
 
-        <p>{{ $t(ability.description) }}</p>
+        <p v-if="ability.description">{{ $t(ability.description) }}</p>
+
+        <img class="ability__demo" :src="ability.demo" />
+      </li>
+    </ul>
+
+    <h3 class="title mt-2">{{ $t("about:combinations") }}:</h3>
+    <ul class="abilities">
+      <li class="ability" v-for="ability in combinations" :key="ability.name">
+        <div class="ability__box">
+          <h2 class="ability__name">{{ $t(ability.name) }}</h2>
+          <p v-if="ability.type" class="ability__type">
+            {{ $t(ability.type) }}
+          </p>
+        </div>
+
+        <ul class="ability__box">
+          <li v-for="(spell, index) in ability.spells" :key="spell">
+            <div class="ability__spell">
+              <img
+                v-if="spell.includes('/')"
+                class="ability__spell-img"
+                :src="spell"
+                :alt="ability.name"
+              />
+              <p v-else class="ability__spell-hit">{{ $t(spell) }}</p>
+
+              <p
+                v-if="index < ability.spells.length - 1"
+                class="ability__spell-plus"
+              >
+                +
+              </p>
+            </div>
+          </li>
+        </ul>
+
+        <p v-if="ability.description">{{ $t(ability.description) }}</p>
 
         <img class="ability__demo" :src="ability.demo" />
       </li>
@@ -86,7 +181,6 @@ ul {
 
 .abilities {
   display: flex;
-  /* flex-direction: column; */
   justify-content: center;
   flex-wrap: wrap;
   gap: 0.5rem;
@@ -98,6 +192,7 @@ ul {
   justify-content: center;
   align-items: center;
   gap: 1rem;
+  width: 45rem;
 
   padding: 1rem;
   border-radius: 1rem;
@@ -117,16 +212,20 @@ ul {
 
 .ability__spell {
   display: flex;
-  flex-direction: column;
   align-items: center;
   gap: 1rem;
 }
 .ability__spell-img {
-  height: 10rem;
+  height: 8rem;
   width: min-content;
 }
+
+.ability__spell-hit {
+  font-size: 5rem;
+}
+
 .ability__demo {
-  height: 25rem;
+  height: 20rem;
   border-radius: 1rem;
 }
 
